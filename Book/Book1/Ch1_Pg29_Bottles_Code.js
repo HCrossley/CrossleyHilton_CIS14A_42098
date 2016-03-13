2aef49ef4aea8dc55c2c04270334850aa7958ca7
HCrossley <hcrossley@student.rcc.edu>
2016-03-13 10:51:54 -0700

    Signed-off-by: HCrossley <hcrossley@student.rcc.edu>

--- Book/Book1/Ch1_Pg29_Bottles_Code.js
+++ Book/Book1/Ch1_Pg29_Bottles_Code.js
@@ -1,7 +1,6 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author: Hilton Crossley
+ * Purpose: Code for Ch1_Pg29_Bottles using while loops.
  */
 
 var word = "bottles";
